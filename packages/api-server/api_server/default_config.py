dffb1ed115a4f22bea5ce5ec4fa6be8cbc203c64
tannhat22 <nguyentannhat2298@gmail.com>
2024-10-10 11:36:39 +0700

    sửa lỗi load font từ threejs

--- packages/api-server/api_server/default_config.py
+++ packages/api-server/api_server/default_config.py
@@ -2,16 +2,16 @@
 config = {
     # ip or hostname to bind the socket to, this only applies when running the server in
     # standalone mode.
-    "host": "127.0.0.1",
-    # "host": "192.168.47.128",
+    # "host": "127.0.0.1",
+    "host": "10.7.11.9",
     # port to bind to, this only applies when running the server in standalone mode.
     "port": 8000,
     "db_url": "sqlite://:memory:",
     # url that rmf-server is being served on.
     # When being a proxy, this must be the url that rmf-server is mounted on.
     # E.g. https://example.com/rmf/api/v1
-    "public_url": "http://localhost:8000",
-    # "public_url": "http://192.168.47.128:8000",
+    # "public_url": "http://localhost:8000",
+    "public_url": "http://10.7.11.9:8000",
     "cache_directory": "run/cache",  # The directory where cached files should be stored.
     "log_level": "WARNING",  # https://docs.python.org/3.8/library/logging.html#levels
     # a user that is automatically given admin privileges, note that this does not guarantee that the user exists in the identity provider.
@@ -24,8 +24,8 @@ config = {
     # NOTE: This is ONLY used for documentation purposes, the "jwt_public_key" will be the
     # only key used to verify a token.
     # "oidc_url": None,
-    "oidc_url": "http://localhost:8080/realms/rmf-web/.well-known/openid-configuration",
-    # "oidc_url": "http://192.168.47.128:8080/realms/rmf-web/.well-known/openid-configuration",
+    # "oidc_url": "http://localhost:8080/realms/rmf-web/.well-known/openid-configuration",
+    "oidc_url": "http://10.7.11.9:8080/realms/rmf-web/.well-known/openid-configuration",
     # Audience the access token is meant for. Can also be an array.
     # Used to verify the "aud" claim.
     # "aud": "rmf_api_server",
@@ -34,8 +34,8 @@ config = {
     # Used to verify the "iss" claim
     # If iss is set to None, it means that authentication should be disabled
     # "iss": None,
-    "iss": "http://localhost:8080/realms/rmf-web",
-    # "iss": "http://192.168.47.128:8080/realms/rmf-web",
+    # "iss": "http://localhost:8080/realms/rmf-web",
+    "iss": "http://10.7.11.9:8080/realms/rmf-web",
     # list of arguments passed to the ros node, "--ros-args" is automatically prepended to the list.
     # e.g.
     #   Run with sim time: ["-p", "use_sim_time:=true"]
